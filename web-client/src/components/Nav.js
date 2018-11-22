f13bf05b125f25f3a9c9e011edd90f6934526b5e
Roman Kaspar <roman@salsitasoft.com>
2018-11-22 10:30:19 +0100

    mui styles for header and navigation

--- web-client/src/components/Nav.js
+++ web-client/src/components/Nav.js
@@ -1,15 +1,36 @@
 import React from 'react';
-import { Link } from 'react-router5';
+import { connect } from 'react-redux';
+import { actions as routerActions } from 'redux-router5';
+import Button from '@material-ui/core/Button';
 
 import { routes } from '../router/routes';
 import './Nav.css';
 
-export const Nav = () => (
-  <nav>
-    {
-      routes.filter((item) => (item.inMenu)).map((item) => (
-        <Link key={item.name} routeName={item.name}>{item.menuText}</Link>
-      ))
-    }
-  </nav>
-);
+const NavView = (props) => {
+  const { routeName, onClick } = props;
+  return (
+    <nav>
+      {
+        routes.filter((item) => (item.inMenu)).map((item) => (
+          <Button
+            key={item.name}
+            size="small"
+            variant="outlined"
+            color={(item.name === routeName) ? 'primary' : 'default'}
+            onClick={() => { onClick(item.name); }}
+          >{item.menuText}</Button>
+        ))
+      }
+    </nav>
+  );
+};
+
+const mapStateToProps = (state) => ({
+  routeName: state.router.route.name,
+});
+
+const mapDispatchToProps = (dispatch) => ({
+  onClick: (routeName) => { dispatch(routerActions.navigateTo(routeName)); },
+});
+
+export const Nav = connect(mapStateToProps, mapDispatchToProps)(NavView);
