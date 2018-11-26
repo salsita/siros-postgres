5e57f8d3d8d493abe01ca7107178a85719ce3d2d
Roman Kaspar <roman@salsitasoft.com>
2018-11-26 18:29:23 +0100

    mui styles for hw-list, sm resolution and up

--- web-client/src/components/hw-list/HwListItemPara.js
+++ web-client/src/components/hw-list/HwListItemPara.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import Typography from '@material-ui/core/Typography';
 
-export const HwListItemSmallPara = (props) => {
+export const HwListItemPara = (props) => {
   const { header, text } = props;
   return (
     <div>
       <Typography color="textSecondary">{header}</Typography>
-      <Typography className="hw-list-item-small-para" color="textPrimary">{text}</Typography>
+      <Typography className="hw-list-item-para" color="textPrimary">{text}</Typography>
     </div>
   );
 };
--- web-client/src/components/hw-list/HwListItemSmallExpanded.js
+++ web-client/src/components/hw-list/HwListItemSmallExpanded.js
@@ -5,23 +5,23 @@ import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
 import Button from '@material-ui/core/Button';
 
-import { HwListItemSmallPara } from './HwListItemSmallPara';
+import { HwListItemPara } from './HwListItemPara';
 
 export const HwListItemSmallExpanded = (props) => {
   const { hwItem, cardClass, onClick } = props;
   return (
     <Card className={cardClass}>
       <CardContent className="hw-list-item-small-expanded">
-        <HwListItemSmallPara header="category:" text={hwItem.category} />
-        <HwListItemSmallPara header="description:" text={hwItem.description} />
-        {!hwItem.active && <HwListItemSmallPara header="active:" text="no" />}
-        {hwItem.active && <HwListItemSmallPara header="on marketplace:" text={hwItem.available ? 'yes' : 'no'} />}
-        <HwListItemSmallPara header="purchase date:" text={hwItem.purchase_date} />
-        <HwListItemSmallPara header="purchase price:" text={hwItem.purchase_price} />
-        <HwListItemSmallPara header="purchased in:" text={hwItem.store} />
-        <HwListItemSmallPara header="condition:" text={hwItem.condition} />
-        {hwItem.active && <HwListItemSmallPara header="current price:" text={hwItem.current_price} />}
-        {hwItem.comment && <HwListItemSmallPara header="comment:" text={hwItem.comment} />}
+        <HwListItemPara header="category:" text={hwItem.category} />
+        <HwListItemPara header="description:" text={hwItem.description} />
+        {!hwItem.active && <HwListItemPara header="active:" text="no" />}
+        {hwItem.active && <HwListItemPara header="on marketplace:" text={hwItem.available ? 'yes' : 'no'} />}
+        <HwListItemPara header="purchase date:" text={hwItem.purchase_date} />
+        <HwListItemPara header="purchase price:" text={hwItem.purchase_price} />
+        <HwListItemPara header="purchased in:" text={hwItem.store} />
+        <HwListItemPara header="condition:" text={hwItem.condition} />
+        {hwItem.active && <HwListItemPara header="current price:" text={hwItem.current_price} />}
+        {hwItem.comment && <HwListItemPara header="comment:" text={hwItem.comment} />}
       </CardContent>
       <CardActions className="small-card-actions">
         <Button size="small" onClick={onClick}>Show less</Button>
