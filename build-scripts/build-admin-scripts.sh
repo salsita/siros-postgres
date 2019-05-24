29b2168f573503b0ad37453db6a77f1b0ed0e838
Roman Kaspar <roman@salsitasoft.com>
2019-05-24 13:44:38 +0200

    admin-scripts: add empty protocols directory into distribution packages

--- build-scripts/build-admin-scripts.sh
+++ build-scripts/build-admin-scripts.sh
@@ -7,10 +7,12 @@ mkdir -p ../build
 rm -f ../build/siros-admin-scripts*.tgz
 PACKAGE_NIX="siros-admin-scripts@${npm_package_version}-nix.tgz"
 PACKAGE_WIN="siros-admin-scripts@${npm_package_version}-win.zip"
+rm -rf protocols
+mkdir protocols
 echo "creating $PACKAGE_NIX ..."
-COPYFILE_DISABLE=1 tar cfz ../build/$PACKAGE_NIX --exclude='bin/siros*.cmd' --exclude='bin/siros-hw-discard' bin/siros* fonts images lib scripts node_modules
+COPYFILE_DISABLE=1 tar cfz ../build/$PACKAGE_NIX --exclude='bin/siros*.cmd' --exclude='bin/siros-hw-discard' bin/siros* fonts images lib scripts node_modules protocols
 echo "... done" && echo ""
 echo "creating $PACKAGE_WIN ..."
-COPYFILE_DISABLE=1 zip -r ../build/$PACKAGE_WIN --exclude='bin/siros-hw-discard.cmd' bin/siros*.cmd fonts images lib scripts node_modules 1>/dev/null
+COPYFILE_DISABLE=1 zip -r ../build/$PACKAGE_WIN --exclude='bin/siros-hw-discard.cmd' bin/siros*.cmd fonts images lib scripts node_modules protocols 1>/dev/null
 echo "... done" && echo ""
 npm ci --no-production
